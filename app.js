9cefd12676b189fe82e09389ec45c55d6797b5ad
KrishnaRaj1363 <155522449+KrishnaRaj1363@users.noreply.github.com>
2024-02-02 23:17:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ const products = [
     colors: [
       {
         code: "black",
-        img: "/img/air.png",
+        img: "./img/air.png",
       },
       {
         code: "darkblue",
-        img: "/img/air2.png",
+        img: "./img/air2.png",
       },
     ],
   },
@@ -24,11 +24,11 @@ const products = [
     colors: [
       {
         code: "lightgray",
-        img: "/img/jordan.png",
+        img: "./img/jordan.png",
       },
       {
         code: "green",
-        img: "/img/jordan2.png",
+        img: "./img/jordan2.png",
       },
     ],
   },
@@ -39,11 +39,11 @@ const products = [
     colors: [
       {
         code: "lightgray",
-        img: "/img/blazer.png",
+        img: "./img/blazer.png",
       },
       {
         code: "green",
-        img: "/img/blazer2.png",
+        img: "./img/blazer2.png",
       },
     ],
   },
@@ -54,11 +54,11 @@ const products = [
     colors: [
       {
         code: "black",
-        img: "/img/crater.png",
+        img: "./img/crater.png",
       },
       {
         code: "lightgray",
-        img: "/img/crater2.png",
+        img: "./img/crater2.png",
       },
     ],
   },
@@ -69,11 +69,11 @@ const products = [
     colors: [
       {
         code: "gray",
-        img: "/img/hippie.png",
+        img: "./img/hippie.png",
       },
       {
         code: "black",
-        img: "/img/hippie2.png",
+        img: "./img/hippie2.png",
       },
     ],
   },
